a6f6609f18b838e29cea058df23eb940f93b42c1
Klimov.A <klimovaleksanr@gmail.com>
2025-04-19 00:15:55 +0700

    Update Vite configuration for improved server settings
    - Change server host setting to true for automatic binding.
    - Add allowed hosts to enhance accessibility for local development and testing.
    - Retain the port setting for consistency with previous configurations.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173 // или порт, который вы используете
+    host: true,
+    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'task-flow.site'],
+    port: 5173,
   }
 })
